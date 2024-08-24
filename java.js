a93ddc7d74e3a979427babce0fde14896aa04708
BhaktiKushan <BhaktiKushan>
2024-08-24 22:17:49 +0530

    hi

---
+++ java.js
@@ -0,0 +1,7 @@
+document.getElementById("button").addEventListener('mouseover',function(){
+    this.style.background = "blue";
+})
+
+document.getElementById("button").addEventListener('mouseout',function(){
+    this.style.background = "yellow";
+})
